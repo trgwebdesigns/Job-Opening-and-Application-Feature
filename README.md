3b286f071c64e2fd86be9ecb2dd9ad97a9604dd2
tomgreer <tom.greer@trgwebdesigns.com>
2022-06-23 23:04:24 -0400

    Rewrite using Divi Theme Builder
    The original version had PHP files for the single, archive, and taxonomy. With the introduction of the Divi Theme Builder, these are no longer needed. In addition, this has been repackaged as a plugin to create the custom post type and taxonomy.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Job Opening and Application Feature
+
+Adds a custom post type for Job Openings and the ability for website visitors apply for jobs.
+
+The plugin creates a Job Openings custom post type and a Job Category taxonomy. It also dynamically inserts an Apply Now button after the content of all Job Opening posts that includes a link to the job application page that includes the job_title as a query string parameter.
+
+To complete the feature additional work needs to completed:
+
+* Create a job application form in Gravity Forms.
+  * Dynamically populate the Position Applying For field with the job_title URL parameter.
+* Create a Job Application page and embed the Job Application form.
+  * The slug for the page needs to be 'job-application' (in order for the Apply Now button to properly link to the application page).
+* Create a Job Openings page that uses a Blog Module to display Job Opening posts.
+* In the Divi Theme Builder:
+  * Create a template for All Job Openings. The only requirement is that it contain a Post Content Module.
+  * Create a template for Job Opening Categories. Use a Blog Module that has the Posts for Current Page set to 'Yes'.
---
+++ trg-job-openings.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Plugin Name: Job Openings and Applications
+Description: Adds a custom post type for Job Openings and the ability for website visitors apply for jobs.
+Author: Tom Greer
+Author URI: http://trgwebdesigns.com
+*/
+
+/**
+ * Register the Job Opening custom post type
+ */
+function trg_job_opening_cpt() {
+    register_post_type( 'trg_job_opening', 
+        array(
+            'labels' => array(
+                'name'               => 'Job Openings',
+                'singular_name'      => 'Job Opening',
+                'menu_name'          => 'Job Openings',
+                'name_admin_bar'     => 'Job Opening',
+                'add_new'            => 'Add New',
+                'add_new_item'       => 'Add New Job Opening',
+                'new_item'           => 'New Job Opening',
+                'edit_item'          => 'Edit Job Opening',
+                'view_item'          => 'View Job Opening',
+                'all_items'          => 'All Job Openings',
+                'search_items'       => 'Search Job Openings',
+                'not_found'          => 'No Job Openings found.',
+                'not_found_in_trash' => 'No Job Openings found in Trash.'
+            ),
+        'description' => 'Job openings',
+        'public' => true,
+        'menu_position' => 6,
+        'menu_icon' => 'dashicons-id',
+        'has_archive' => false,
+        'rewrite' => array(
+            'slug' => 'job_opening',
+            'with_front' => false,
+        ),
+        'show_in_rest' => true,
+        'supports' => array( 'title', 'editor', 'excerpt', 'revisions', 'page-attributes', )
+        )
+    );
+}
+
+add_action( 'init', 'trg_job_opening_cpt' );
+
+/**
+ * Register the Job Category Taxonomy
+ */
+function trg_job_category_taxonomy() {
+    register_taxonomy( 'trg_job_category', 'trg_job_opening', 
+        array(
+            'hierarchical' => true,
+            'labels' => array(
+                'name'               => 'Job Category',
+                'singular_name'      => 'Job Category',
+                'menu_name'          => 'Job Categories',
+                'name_admin_bar'     => 'Job Category',
+                'add_new'            => 'Add New',
+                'add_new_item'       => 'Add New Job Category',
+                'new_item'           => 'New Job Category',
+                'edit_item'          => 'Edit Job Category',
+                'view_item'          => 'View Job Category',
+                'all_items'          => 'All Job Categories',
+                'search_items'       => 'Search Job Categories',
+                'parent_item_colon'  => 'Parent Job Categories:',
+                'not_found'          => 'No Job Categories found.',
+                'not_found_in_trash' => 'No Job Categories found in Trash.'
+            ),
+            'query_var' => true,
+            'rewrite' => array(
+            'slug' => 'job_category',
+            'with_front' => false
+            )
+        )
+    );
+}
+add_action( 'init', 'trg_job_category_taxonomy');
+
+/**
+ * On plugin installation, we need to flush the rewrite rules after registering new custom post types.
+ */
+function trg_job_openings_activate() {
+    flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'trg_job_openings_activate' );
+?>
